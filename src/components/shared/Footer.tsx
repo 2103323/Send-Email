c77f73fdd33a796018a3a7383fb012d6a36dddcc
suyash <98270897+2103323@users.noreply.github.com>
2024-07-24 20:20:46 +0530

    Update Footer.tsx

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="border-t">
     
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
-        <p>2024 Frover Labs. All Rights reserved.</p>
+        <p>2024 Company. All Rights reserved.</p>
       </div>
       
     </footer>
